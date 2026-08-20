46044adc82ceadb6b6dff971fdf1d2bd0407f43a
Ramkrishna <rupambarmanxyz@gmail.com>
2026-08-20 22:13:33 +0530

    iam policy: v1

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -32,3 +32,32 @@ resource "aws_iam_role" "ecs_task" {
     }]
   })
 }
+
+# --- GitHub Actions OIDC provider + deploy role (no long-lived AWS keys in CI) ---
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name = "${var.project_name}-github-actions-deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          # Restrict to your repo - update org/repo before applying
+          "token.actions.githubusercontent.com:sub" = "repo:YOUR_GH_ORG/YOUR_REPO:*"
+        }
+      }
+    }]
+  })
+}
